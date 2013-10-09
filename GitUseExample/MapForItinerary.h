fa24ed54a74fe0d19ab3a153d63a9aa76d0333d3
dmarwil <dmarwil@alumni.uidaho.edu>
2013-10-09 11:51:57 -0600

    Is this going into another branch?

---
+++ GitUseExample/MapForItinerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.h
+//  GitUseExample
+//
+//  Created by David Marwil on 10/9/13.
+//  Copyright (c) 2013 David Marwil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForItinerary : NSObject
+
+@end
---
+++ GitUseExample/MapForItinerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.m
+//  GitUseExample
+//
+//  Created by David Marwil on 10/9/13.
+//  Copyright (c) 2013 David Marwil. All rights reserved.
+//
+
+#import "MapForItinerary.h"
+
+@implementation MapForItinerary
+
+@end
