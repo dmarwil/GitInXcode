ca49c0136037395814aa2e12bdf0f9996c596c0d
dmarwil <dmarwil@alumni.uidaho.edu>
2013-10-09 11:52:39 -0600

    test to the map branch

---
+++ GitUseExample/GITMap.h
@@ -0,0 +1,13 @@
+//
+//  GITMap.h
+//  GitUseExample
+//
+//  Created by David Marwil on 10/9/13.
+//  Copyright (c) 2013 David Marwil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GITMap : NSObject
+
+@end
---
+++ GitUseExample/GITMap.m
@@ -0,0 +1,13 @@
+//
+//  GITMap.m
+//  GitUseExample
+//
+//  Created by David Marwil on 10/9/13.
+//  Copyright (c) 2013 David Marwil. All rights reserved.
+//
+
+#import "GITMap.h"
+
+@implementation GITMap
+
+@end
