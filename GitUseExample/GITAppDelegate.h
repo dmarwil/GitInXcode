81a498b7b19c843a8ef30bada5318d27440e75b2
dmarwil <dmarwil@alumni.uidaho.edu>
2013-10-09 11:40:55 -0600

    Initial Commit

---
+++ GitUseExample/GITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GITAppDelegate.h
+//  GitUseExample
+//
+//  Created by David Marwil on 10/9/13.
+//  Copyright (c) 2013 David Marwil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/GITViewController.m
@@ -0,0 +1,29 @@
+//
+//  GITViewController.m
+//  GitUseExample
+//
+//  Created by David Marwil on 10/9/13.
+//  Copyright (c) 2013 David Marwil. All rights reserved.
+//
+
+#import "GITViewController.h"
+
+@interface GITViewController ()
+
+@end
+
+@implementation GITViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
